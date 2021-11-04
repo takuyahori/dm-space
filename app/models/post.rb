a57b86e64522e25be97398544ac60d0a6f3be640
takuyahori <takuyahori0904@gmail.com>
2021-11-04 17:37:03 +0900

    postsテーブルバリデーション記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :cliant
   has_one_attached :image
+
+  with_option presence: true do
+    validates :title
+    validates :today
+    validates :explanation
+    validates :image
+  end
 end
