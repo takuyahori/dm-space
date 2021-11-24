de5fc45b55ea3d0c00b93e7409278cbd0f29b09e
takuyahori <takuyahori0904@gmail.com>
2021-11-24 16:02:29 +0900

    コメント投稿者のみ削除できる記述

--- app/controllers/cliants/registrations_controller.rb
+++ app/controllers/cliants/registrations_controller.rb
@@ -52,7 +52,7 @@ class Cliants::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    posts_path(resources)
+    posts_path
   end
 
   # The path used after sign up for inactive accounts.
