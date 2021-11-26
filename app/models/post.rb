e3ac27eb8382a01c696f4a8ef70b57f32a18c7ee
takuyahori <takuyahori0904@gmail.com>
2021-11-26 16:30:55 +0900

    アソシエーション&いいね判別

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,13 @@ class Post < ApplicationRecord
   belongs_to :cliant
   has_many_attached :images
   has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
-  has_many :liked_cliants, through: :likes, source: :cliant
-  has_many :liked_trainers, through: :likes, source: :cliant
+  has_many :likes
+  has_many :cliant, through: :likes
+  has_many :trainer, through: :likes
+
+  def liked_by?(cliant)
+    likes.where(cliant_id: cliant.id).exists?
+  end
 
   with_options presence: true do
     validates :title
