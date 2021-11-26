f67946d3ca3ef7952577285e3009544841df8bb3
takuyahori <takuyahori0904@gmail.com>
2021-11-26 16:29:57 +0900

    テーブル修正

--- db/migrate/20211124070925_create_likes.rb
+++ db/migrate/20211124070925_create_likes.rb
@@ -1,9 +1,9 @@
 class CreateLikes < ActiveRecord::Migration[6.0]
   def change
     create_table :likes do |t|
-      t.references :post, null: false, foreign_key: true
-      t.references :cliant, null: false, foreign_key: true
-      t.references :trainer, null: false, foreign_key: true
+      t.references :post, foreign_key: true
+      t.references :cliant, foreign_key: true
+      t.references :trainer, foreign_key: true
       t.timestamps
     end
   end
