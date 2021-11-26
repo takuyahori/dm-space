bea3d2aa46d30623efb413e4f4a68c279301b097
takuyahori <takuyahori0904@gmail.com>
2021-11-26 16:31:15 +0900

    コントローラー記述

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,22 @@
 class LikesController < ApplicationController
+  before_action :set_like
+
+  def create
+    cliant = current_cliant
+    post = Post.find(params[:post_id])
+    like = Like.create(cliant_id: cliant.id, post_id: post.id)
+  end
+
+  def destroy
+    cliant = current_cliant
+    post = Post.find(params[:post_id])
+    like = Like.find_by(cliant_id: cliant.id, post_id: post.id)
+    like.delete
+  end
+
+  private
+
+  def set_like
+    @post = Post.find(params[:post_id])
+  end
 end
