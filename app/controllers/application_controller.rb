9fef9d4b09084fd75521dc8ddef480c4db03e86a
takuyahori <takuyahori0904@gmail.com>
2021-12-09 16:03:18 +0900

    記述削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
 
-  def authenticate_any!
-    if cliant_signed_in?
-      true
-    else
-      authenticate_trainer!
-    end
-  end
-
   def after_sign_in_path_for(resource)
     posts_path(resource)
   end
