b4d7c6178aa104a2a56aec085be5b7a4000aff34
takuyahori <takuyahori0904@gmail.com>
2022-01-06 17:16:44 +0900

    mouseover消去

--- app/javascript/header.js
+++ app/javascript/header.js
@@ -3,14 +3,6 @@ window.addEventListener('load', function(){
   const pullDownButton = document.getElementById("lists")
   const pullDownParents = document.getElementById("pull-down")
 
-  pullDownButton.addEventListener('mouseover', function(){
-    this.setAttribute("style", "background-color:#FFBEDA;")
-  })
-
-  pullDownButton.addEventListener('mouseout', function(){
-    this.removeAttribute("style", "background-color:#FFBEDA;")
-  })
-
   pullDownButton.addEventListener('click', function(){
     if (pullDownParents.getAttribute("style") == "display:block;") {
       pullDownParents.removeAttribute("style", "display:block;")
